e213f7445d9aeab6d76c63e36c235d6499ca219f
muzixikali <3468485032@qq.com>
2022-07-13 14:54:55 +0800

    users命名空间

--- bookshop/bookshop/apps/users/urls.py
+++ bookshop/bookshop/apps/users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     # 用户注册路由
-    path('register/', views.RegisterView.as_view())
+    path('register/', views.RegisterView.as_view(), name='register')
 ]
--- bookshop/bookshop/urls.py
+++ bookshop/bookshop/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # users子应用
-    path('users/', include('users.urls'))
+    path('users/', include(('users.urls','users'),namespace="users"))
 ]
